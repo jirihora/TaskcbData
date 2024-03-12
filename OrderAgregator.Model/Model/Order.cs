4402eb53ec80d32c63fb75d619609b7b4f8d5970
Jiří Hora <jhora@trask.cz>
2024-03-12 15:25:06 +0100

    WIP: Add OrderController; Add model lib

---
+++ OrderAgregator.Model/Model/Order.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OrderAgregator.Model.Model
+{
+    public class Order
+    {
+        [JsonPropertyName("productId")]
+        public string ProductId { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+    }
+}
---
+++ OrderAgregatorAPI/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderAgregator.Model.Model;
+using System.Threading.Tasks;
+
+namespace OrderAgregatorAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> UploadOrders(Order[] orders)
+        {
+
+
+
+            return new AcceptedResult();
+        }
+    }
+}
